6970d576c073c93c5cc74fe2d24dc131c39a19b4
Chromiumore <parts.dima06@gmail.com>
2026-05-30 00:47:37 +0300

    kafka telemetry

--- IndDevTelemetryService/src/main/java/me/chromiumore/satsystem/service/TelemetryServiceGrpcImpl.java
+++ IndDevTelemetryService/src/main/java/me/chromiumore/satsystem/service/TelemetryServiceGrpcImpl.java
@@ -1,29 +1,37 @@
 package me.chromiumore.satsystem.service;
 
 import io.grpc.stub.StreamObserver;
+import lombok.RequiredArgsConstructor;
 import me.chromiumore.satsystem.Telemetry;
 import me.chromiumore.satsystem.TelemetryServiceGrpc;
 import net.devh.boot.grpc.server.service.GrpcService;
 
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+@RequiredArgsConstructor
 @GrpcService
 public class TelemetryServiceGrpcImpl extends TelemetryServiceGrpc.TelemetryServiceImplBase {
     private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
     private final Random random = new Random();
-
-    private static final List<Long> SATELLITE_IDS = List.of(1L, 2L, 3L);
+    private final SatelliteIdRepository satelliteIdRepository;
 
     @Override
     public void streamTelemetry(Telemetry.TelemetryRequest request, StreamObserver<Telemetry.TelemetryUpdate> responseObserver) {
         scheduler.scheduleAtFixedRate(() -> {
             try {
-                Long satId = SATELLITE_IDS.get(random.nextInt(SATELLITE_IDS.size()));
+                Set<Long> activeIds = satelliteIdRepository.getAll();
+                if (activeIds.isEmpty()) return;
+
+                List<Long> idList = new ArrayList<>(activeIds);
+
+                Long satId = idList.get(random.nextInt(idList.size()));
                 Telemetry.TelemetryUpdate update = Telemetry.TelemetryUpdate.newBuilder()
                         .setSatelliteId(satId)
                         .setTemperatureInside(20.0 + random.nextDouble() * 10)
@@ -34,6 +42,6 @@ public class TelemetryServiceGrpcImpl extends TelemetryServiceGrpc.TelemetryServ
             }  catch (Exception e) {
                 responseObserver.onError(e);
             }
-        }, 0, 1, TimeUnit.MINUTES);
+        }, 0, 10, TimeUnit.SECONDS);
     }
 }
